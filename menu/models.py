0a9c9a260185e7523dab68134a4ddb78578f669a
leonardo zurita <zuritarubenleonardo@gmail.com>
2022-10-25 20:33:21 -0300

    clase persona

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+
+    nombre =models.CharField(max_length=10)
+    apellido=models.CharField(max_length=10)
+    fecha_de_nacimiento = models.DateField()
